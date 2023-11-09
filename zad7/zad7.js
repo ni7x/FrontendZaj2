6b0cdd3df4d5510bb5541a1de43eda204b30492d
kpio7 <99970419+ni7x@users.noreply.github.com>
2023-11-09 10:39:36 +0100

    Create zad7.js

---
+++ zad7/zad7.js
@@ -0,0 +1,19 @@
+isHappyNumber = (num) => {
+    let sum = getSumOfSquaredDigits(num);
+    while(sum !== 1){
+        sum = getSumOfSquaredDigits(sum)
+    }
+    return true;
+}
+
+
+getSumOfSquaredDigits = (num) => {
+    return String(num).split("")
+                      .map(Number)
+                      .reduce((a0, a1) => a0 + a1*a1, 0)
+}
+
+
+
+
+console.log(isHappyNumber(19))
